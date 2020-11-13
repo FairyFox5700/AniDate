e87d484ca2f240595ad5631c0a5e96e5b9f264b3
Ira Tyschenko <tyschenk40@gmail.com>
2020-11-13 17:47:25 +0200

    account models created

---
+++ src/Services/Account/Account.API/Entities/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Account.API.Entities
+{
+    public class Image
+    {  
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ImageId { get; set; }
+        public string ImageFileName { get; set; }
+        public string ImageUri { get; set; }
+    }
+}
---
+++ src/Services/Account/Account.API/Entities/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Account.API.Entities
+{
+    public class Pet
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PetId { get; set; }
+        public int UserId { get; set; }
+        public int ImageId { get; set; }
+        public bool IsMail { get; set; }
+        public string Breed { get; set; }
+        public AnimalType AnimalType { get; set; }
+        public string AboutMe { get; set; }
+    }
+
+    public enum AnimalType
+    {
+        Cat,
+        Dog
+    }
+}
