297cb793a44a7b5549b95497c483d5c5561d0697
Ira Tyschenko <tyschenk40@gmail.com>
2020-11-13 17:55:45 +0200

    added identity project

---
+++ src/Identity/Identity.API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.API.Entities
+{
+    public class AppUser:IdentityUser<int>
+    {
+        public DateTime BirthDate { get; set; }
+        public string AboutMe { get; set; }
+        public bool IsMail { get; set; }
+        public int ImageId { get; set; }
+        //pets but no list to access!!!!!
+    }
+}
